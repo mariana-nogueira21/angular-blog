4e4cca57e59998f6834c58fb3e93471caefd513a
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2024-10-14 10:31:24 -0300

    Creating the components structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
