bf2015503a7e40cdd055976bf91e579f8be3c9a0
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2024-10-15 15:06:29 -0300

    Styling the big-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent],
+  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
