dcb425e1fb5d0a8afe4d9d499a3ecf9f032441f2
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2024-10-26 20:52:02 -0300

    Starting responsive

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -5,7 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './menu-title.component.html',
-  styleUrl: './menu-title.component.css'
+  styleUrls: [
+    './menu-title.component.css',
+    './menu-title.responsive.component.css'
+  ]
 })
 export class MenuTitleComponent {
 
